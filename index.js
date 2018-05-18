2ae431e41de3480bbbdf1f9015d111856de6ae80
Wcbn38 <37038441+Wcbn38@users.noreply.github.com>
2018-05-18 20:51:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ bot.on('message', (message) => {
         Pts2 = 0
         Pts3 = 0
         Pts4 = 0
-        bot.role.get(ID1).remove(SEND_TTS_MESSAGES)
+        bot.roles.get(ID1).remove(SEND_TTS_MESSAGES)
         if ( Pts1 > Pts2 || Pts1 > Pts3 || Pts1 > Pts4 ) {
         
         } 
