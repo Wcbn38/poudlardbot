3b1832a7f4bd6a59d72addafa39820e09a25370a
Wcbn38 <37038441+Wcbn38@users.noreply.github.com>
2018-05-19 13:01:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -52,31 +52,32 @@ bot.on('ready', (channels) => {
     Pts2 = parseInt(bot.channels.get(IDptsname2).topic, 10)
     Pts3 = parseInt(bot.channels.get(IDptsname3).topic, 10)
     Pts4 = parseInt(bot.channels.get(IDptsname4).topic, 10)
+    console.log(`Pts1 = ${Pts1} ; Pts2 = ${Pts2} ; Pts3 = ${Pts3} ; Pts4 = ${Pts4} ;`)
 })
 
 bot.on('guildMemberAdd', (member, guild) => {
     aleaMaison = getrandom(1, 4);
     console.log(member.user.username);
     if (aleaMaison === 1) {
-        console.log(`${aleaMaison} // ${name1}`);
+        console.log(`MemberAdd : ${member.user.username} // ${aleaMaison} // ${name1}`);
         member.addRole(ID1);
         bot.channels.get(defaultChannel).send(`${member.user.username} a rejoint la faction ${name1}`)
         return
     }
     if (aleaMaison === 2) {
-        console.log(`${aleaMaison} // ${name2}`);
+        console.log(`MemberAdd : ${member.user.username} // ${aleaMaison} // ${name2}`);
         member.addRole(ID2);
         bot.channels.get(defaultChannel).send(`${member.user.username} a rejoint la faction ${name2}`)
         return
     }
     if (aleaMaison === 3) {
-        console.log(`${aleaMaison} // ${name3}`);
+        console.log(`MemberAdd : ${member.user.username} // ${aleaMaison} // ${name3}`);
         member.addRole(ID3);
         bot.channels.get(defaultChannel).send(`${member.user.username} a rejoint la faction ${name3}`)
         return
     }
     if (aleaMaison === 4) {
-        console.log(`${aleaMaison} // ${name4}`);
+        console.log(`MemberAdd : ${member.user.username} // ${aleaMaison} // ${name4}`);
         member.addRole(ID4);
         bot.channels.get(defaultChannel).send(`${member.user.username} a rejoint la faction ${name4}`)
         return
@@ -87,10 +88,12 @@ bot.on('message', (message) => {
     if (message.content === prefix + "help" && message.channel.id === consoleChannel) {
         bot.channels.get(consoleChannel).send(`prefix : ${prefix} \n "startgame" pour lancer une partie \n "annul" pour annuler une partie \n "winner" pour definir le vainqueur \n "event" pour lancer un event \n "eventannul" pour annuler un event \n "eventWinner" pour declarer le gagnant`)
         bot.channels.get(consoleChannel).send(`nom des maisons a rentrer : \n ${name1},${name2},${name3},${name4}`)
+        console.log(`Help called : ${message.member.user.id}`)
         return
     }
     if (message.content === prefix + "pts") {
         message.channel.send(`points des maisons : \n ${name1} : ${Pts1} \n ${name2} : ${Pts2} \n ${name3} : ${Pts3} \n ${name4} : ${Pts4}`)
+        console.log(`Points total called : ${message.member.user.id}`)
         return
     }
     if ( (message.content === prefix + "reset" + mdp) && (message.channel.id === consoleChannel) ) {
@@ -104,59 +107,98 @@ bot.on('message', (message) => {
         bot.channels.get(IDptsname3).setTopic(Pts3.toString(10))
         bot.channels.get(IDptsname4).setTopic(Pts4.toString(10))
         message.delete(10)
+        console.log(`reset succeful : ${message.member.user.id}`)
         return
     }
     if ( (message.content === prefix + "startseason" + mdp) && (message.channel.id === consoleChannel) ) {
         message.delete(10)
-        bot.channels.get(IDptsname1).setTopic(Pts1.toString(10))
-        bot.channels.get(IDptsname2).setTopic(Pts2.toString(10))
-        bot.channels.get(IDptsname3).setTopic(Pts3.toString(10))
-        bot.channels.get(IDptsname4).setTopic(Pts4.toString(10))
+
+        if ( Pts1 > Pts2 && Pts1 > Pts3 && Pts1 > Pts4 ) {
+            message.guild.roles.get(ID1).setPermissions(['SEND_TTS_MESSAGES', 'MANAGE_NICKNAMES'])
+            message.guild.roles.get(ID2).setPermissions([])
+            message.guild.roles.get(ID3).setPermissions([])
+            message.guild.roles.get(ID4).setPermissions([])
+            message.guild.roles.get(ID1).setPosition(10)
+            bot.channels.get(defaultChannel).send(`saison termine, ${name1} a gangne! \n debut d'une nouvelle saison.`)
+            console.log(`Saison termine, ${name1} a gagne, privilege accorde`)
+        } 
+        else {
+        if (Pts2 > Pts4 && Pts2 > Pts3 && Pts2 > Pts4) {
+            message.guild.roles.get(ID1).setPermissions([])
+            message.guild.roles.get(ID2).setPermissions(['SEND_TTS_MESSAGES', 'MANAGE_NICKNAMES'])
+            message.guild.roles.get(ID3).setPermissions([])
+            message.guild.roles.get(ID4).setPermissions([])
+            message.guild.roles.get(ID2).setPosition(10)
+            bot.channels.get(defaultChannel).send(`saison termine, ${name2} a gangne! \n debut d'une nouvelle saison.`)
+            console.log(`Saison termine, ${name2} a gagne, privilege accorde`)
+        } 
+        else {
+        if (Pts3 > Pts1 && Pts2 > Pts2 && Pts21 > Pts4) {
+            message.guild.roles.get(ID1).setPermissions([])
+            message.guild.roles.get(ID2).setPermissions([])
+            message.guild.roles.get(ID3).setPermissions(['SEND_TTS_MESSAGES', 'MANAGE_NICKNAMES'])
+            message.guild.roles.get(ID4).setPermissions([])
+            message.guild.roles.get(ID3).setPosition(10)
+            bot.channels.get(defaultChannel).send(`saison termine, ${name3} a gangne! \n debut d'une nouvelle saison.`)
+            console.log(`Saison termine, ${name3} a gagne, privilege accorde`)
+        } 
+        else {
+        if ( Pts4 > Pts1 && Pts2 > Pts2 && Pts2 > Pts3 ) {
+            message.guild.roles.get(ID1).setPermissions([])
+            message.guild.roles.get(ID2).setPermissions([])
+            message.guild.roles.get(ID3).setPermissions([])
+            message.guild.roles.get(ID4).setPermissions(['SEND_TTS_MESSAGES', 'MANAGE_NICKNAMES'])
+            message.guild.roles.get(ID4).setPosition(10)
+            bot.channels.get(defaultChannel).send(`saison termine, ${name4} a gangné! \n debut d'une nouvelle saison.`)
+            console.log(`Saison termine, ${name4} a gagne, privilege accorde`)
+        }
+        else {
+            message.guild.roles.get(ID1).setPermissions([])
+            message.guild.roles.get(ID2).setPermissions([])
+            message.guild.roles.get(ID3).setPermissions([])
+            message.guild.roles.get(ID4).setPermissions([])
+            bot.channels.get(defaultChannel).send(`Saison termine, pas de gagnant cette fois! \n debut d'une nouvelle saison.`)
+            console.log(`Saison terminé, aucun gagnant, tout les privileges sont annulé.`)
+        }
+        }
+        }
+        }
+        console.log(`Debut d'une nouvelle saison...`)
         Pts1 = 0
         Pts2 = 0
         Pts3 = 0
         Pts4 = 0
-        bot.roles.get(ID1)
-        if ( Pts1 > Pts2 || Pts1 > Pts3 || Pts1 > Pts4 ) {
-        
-        } 
-        else {
-        if ( Pts2 > Pts4 || Pts1 > Pts3 || Pts1 > Pts4 ) {
-        
-        } 
-        else {
-        if ( Pts3 > Pts1 || Pts1 > Pts2 || Pts1 > Pts4 ) {
-        
-        } 
-        else {
-        if ( Pts4 > Pts1 || Pts1 > Pts2 || Pts1 > Pts3 ) {
-        
-        } else {
-        }
-        }}}
+        bot.channels.get(IDptsname1).setTopic(Pts1.toString(10))
+        bot.channels.get(IDptsname2).setTopic(Pts2.toString(10))
+        bot.channels.get(IDptsname3).setTopic(Pts3.toString(10))
+        bot.channels.get(IDptsname4).setTopic(Pts4.toString(10))
+        console.log(`Saison commencé : ${message.member.user.id}`)
+        return
     }
     if (message.channel.id === consoleChannel && message.content !== "faction 1 :" && message.content !== "defi lance : ne pas lancer de nouveaux defi avant le message de fin!" && message.content !== "error : unknown. Var `suivitMaison` crashed. Reebooting... " && message.content !== "faction 2 :" && message.content !== "mise :") {
         if (message.content === prefix + "startgame" && suivitMaison === 0) {
-            console.log("defie lance")
             bot.channels.get(consoleChannel).send("faction 1 :")
             suivitMaison = 1
+            console.log(`defie lance : ${message.member.user.id}`)
             return
         }
         if (message.content === prefix + "annul") {
             bot.channels.get(consoleChannel).send("defi annule")
             suivitMaison = 0
             PtsMisee = 0
+            console.log(`defi annule : ${message.member.user.id}`)
             return
         } else {
             if (message.content === prefix + "winner" && suivitMaison === 4) {
                 bot.channels.get(consoleChannel).send("gagnant :")
-                console.log("declaration gagnant")
+                console.log(`Declaration gagnant : ${message.member.user.id}`)
                 suivitMaison = suivitMaison + 1
                 return
             }
             if ((message.content === maison1 || message.content === maison2) && suivitMaison === 5) {
                 bot.channels.get(defaultChannel).send(`@everyone ${message.content} a gagne`)
                 bot.channels.get(consoleChannel).send(`validation termine!`)
+                console.log(`Gagnant : ${message.content} : ${message.member.user.id}`)
                 if (message.content === maison1) {
                     perdant = maison2
                 } else {
@@ -195,6 +237,7 @@ bot.on('message', (message) => {
                     bot.channels.get(consoleChannel).send("defi lance : ne pas lancer de nouveaux defi avant le message de fin!")
                     bot.channels.get(defaultChannel).send(`Une game entre ${maison1} et ${maison2} a commence pour ${PtsMisee} points. Bonne chance a eux!`)
                     suivitMaison = suivitMaison + 1
+                    console.log(`parametres du defi termine`)
                     return
                 }
                 if (suivitMaison > 5) {
@@ -236,6 +279,7 @@ bot.on('message', (message) => {
             if (message.channel.id === consoleChannel && message.content === `${prefix}event` && suivitEvent === 0) {
                 bot.channels.get(consoleChannel).send(`votre message :`)
                 suivitEvent = 20
+                console.log(`Event : ${message.member.user.id}`)
                 return
             }
             if (suivitEvent === 20 && message.channel.id === consoleChannel && message.content !== "votre message :") {
@@ -249,11 +293,13 @@ bot.on('message', (message) => {
                 bot.channels.get(consoleChannel).send(`Event reussi. Merci de ne pas lancer d'event.`)
                 bot.channels.get(infosChannel).send(`@everyone , nouvel event : ${messageEvent}. \n récompense : ${PtsEvent * 3}`)
                 suivitEvent = 22
+                console.log(`Parametre event termine`)
                 return
             }
             if (suivitEvent === 22 && message.channel.id === consoleChannel && message.content === `${prefix}eventWinner` && message.content !== "Event reussi. Merci de ne pas lancer d'event.") {
                 bot.channels.get(consoleChannel).send(`winner :`)
                 suivitEvent = 23
+                console.log(`declaration event winner : ${message.member.user.id}`)
                 return
             }
             if (suivitEvent === 23 && message.channel.id === consoleChannel && (message.content === name1 || message.content === name2 || message.content === name3 || message.content === name4)) {
@@ -290,6 +336,7 @@ bot.on('message', (message) => {
                 bot.channels.get(IDptsname2).setTopic(Pts2.toString(10))
                 bot.channels.get(IDptsname3).setTopic(Pts3.toString(10))
                 bot.channels.get(IDptsname4).setTopic(Pts4.toString(10))
+                console.log(`pts event distribué`)
                 return
             }
             if (message.channel.id === consoleChannel && message.content === `${prefix}eventannul`) {
@@ -297,6 +344,7 @@ bot.on('message', (message) => {
                 messageEvent = ""
                 suivitEvent = 0
                 PtsEvent = 0
+                console.log(`event annule : ${message.member.user.id}`)
             }
         }
     }
