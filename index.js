8d2fc5a2adc095cf6ba35460c9972af29fd16d71
Wcbn38 <37038441+Wcbn38@users.noreply.github.com>
2018-06-07 20:37:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const mainCategory = process.env.mainCategory
 
 var http = require("http");
 setInterval(function () {
-http.get("https://wcbn-s-bot.herokuapp.com/");
+http.get("http://wcbn-s-bot.herokuapp.com/");
 },9000)
 
 setInterval(function () {
